36a4400c3d3ee4a106e3921d636a9db8e2c6f14e
Оспанов Олег <oleg-130@mail.ru>
2025-11-17 11:35:24 +0500

    add yolo start detected person

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import cv2
+from ultralytics import YOLO
 
-def main(path):
-    cap = cv2.VideoCapture(path)
+video_path = "test.mp4"
+model = YOLO("yolov8s.pt")
+cap = cv2.VideoCapture(video_path)
+exit_zone = (930, 400, 1250, 700)
 
+
+def main():
+    i = 0
     if not cap.isOpened():
         print("Не удалось открыть видеофайл")
         return
@@ -12,7 +18,17 @@ def main(path):
         if not ret:
             break
 
-        cv2.imshow("Video", frame)
+        i += 1
+        if i % 10 != 0:
+            continue
+
+        results = model.predict(frame)
+        annotated_frame = results[0].plot()
+
+        x1, y1, x2, y2 = exit_zone
+
+        cv2.rectangle(annotated_frame,  (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.imshow("Video", annotated_frame)
 
 
         if cv2.waitKey(25) & 0xFF == 27:
@@ -23,5 +39,4 @@ def main(path):
 
 
 if __name__ == "__main__":
-    video_path = "test.mp4"
-    main(video_path)
+    main()
