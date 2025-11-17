f7c1ae8c052388e5a5efbfbfcf84677006bbe579
Оспанов Олег <oleg-130@mail.ru>
2025-11-17 12:33:13 +0500

    add alert function

--- src/main.py
+++ src/main.py
@@ -4,8 +4,12 @@ from ultralytics import YOLO
 video_path = "test.mp4"
 model = YOLO("yolov8s.pt")
 cap = cv2.VideoCapture(video_path)
-exit_zone = (930, 400, 1250, 700)
+alert_zone = (930, 400, 1250, 700)
 
+
+def alert(px, py, rect):
+    x1, y1, x2, y2 = rect
+    return x1 <= px <= x2 and y1 <= py <= y2
 
 def main():
     i = 0
@@ -25,12 +29,26 @@ def main():
         results = model.predict(frame)
         annotated_frame = results[0].plot()
 
-        x1, y1, x2, y2 = exit_zone
+        for r in results:
+            for box in r.boxes:
+                cls = int(box.cls)
+                if cls != 0:
+                    continue
 
-        cv2.rectangle(annotated_frame,  (x1, y1), (x2, y2), (0, 0, 255), 2)
+                x1b, y1b, x2b, y2b = map(int, box.xyxy[0])
+                cx = int((x1b + x2b) / 2)
+                cy = int((y1b + y2b) / 2)
+
+                x1, y1, x2, y2 = alert_zone
+                cv2.rectangle(annotated_frame,  (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.circle(annotated_frame, (cx, cy), 4, (255, 0, 0), -1)
+
+                if alert(cx, cy, alert_zone):
+                    cv2.putText(annotated_frame, "ALERT!", (50, 50),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    print("ALERT!")
+
         cv2.imshow("Video", annotated_frame)
-
-
         if cv2.waitKey(25) & 0xFF == 27:
             break
 
